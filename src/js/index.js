e9419d0bb63cbd39994a22c225be52e27fd0ed33
maxiu271299 <maksymilian.kierski@gmail.com>
2020-02-18 23:33:41 +0100

    Add axios library and communicate with API

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,14 @@
-import mainView from './views/mainView';
-const x = 23;
-console.log(`this is that number: ${mainView} and ${x}`);
-console.log('Hello world!');
+import axios from 'axios';
+
+async function getResults(query) {
+    try {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        for ( let i =0; i< res.data.count; i++) {
+            console.log(res.data.recipes[i]);
+        }
+    } catch (e) {
+        alert(e);
+    }
+}
+let query = prompt('What kind of recipe are you looking?');
+getResults(query);
---
+++ src/js/models/Search.js
@@ -0,0 +1,2 @@
+//const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+//const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
