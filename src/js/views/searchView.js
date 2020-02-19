f2fcdeb690a96c421c40ff9f621155bf94b57a15
maxiu271299 <maksymilian.kierski@gmail.com>
2020-02-19 15:15:10 +0100

    Add splitting too long title recipes algorithm

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -11,6 +11,33 @@ export const clearInput = () => {
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 };
+/*
+//    0    1     2    3     4
+//'Pasta with tomato and spinach'
+array length 5
+                   5
+acc: 0 / acc + cur.length = 5 newTitle =['Pasta'],
+                   4
+acc: 5 / acc + cur.length = 9 newTitle =['Pasta', with],
+                   6
+acc: 9 / acc + cur.length = 15 newTitle =['Pasta', 'with', 'tomato'],
+                   3
+acc: 15 / acc + cur.length = 18 newTitle =['Pasta', 'with', 'tomato'],
+acc: 18 / acc + cur.length = 18 newTitle =['Pasta', 'with', 'tomato'],
+ */
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if(title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= 17) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+        return `${newTitle.join(' ')}...`
+    }
+    return title;
+};
 
 const renderRecipe = recipe => {
     const element =
@@ -21,7 +48,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
